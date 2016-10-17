dcfe027c892759ccbcd3e5061d8f06f0b3315fb9
Kia Farhang <kfarhang0@gmail.com>
2016-10-17 14:34:26 -0700

    Add files via upload

---
+++ graphite.js
@@ -0,0 +1,57 @@
+var domWalker = document.createTreeWalker(document.documentElement, NodeFilter.SHOW_ELEMENT);
+
+while (domWalker.nextNode()) {
+    checkBackground(domWalker.currentNode);
+    checkColor(domWalker.currentNode);
+
+    if (domWalker.currentNode.tagName == 'P') {
+        checkLineHeight(domWalker.currentNode);
+    }
+
+    function checkBackground(node) {
+
+        var thisNodeBackground = window.getComputedStyle(node)['background'];
+        var thisNodeBackgroundColor = window.getComputedStyle(node)['background-color'];
+
+        if (thisNodeBackground == 'rgb(255, 255, 255)' || thisNodeBackgroundColor == 'rgb(255, 255, 255)') {
+            domWalker.currentNode.style.backgroundColor = '#FAFAFA';
+        }
+        if (thisNodeBackground == 'rgb(250, 250, 250)' || thisNodeBackgroundColor == 'rgb(250, 250, 250)') {
+            domWalker.currentNode.style.backgroundColor = '#FFFFFF';
+        }
+
+        if (thisNodeBackground == 'rgb(0, 0, 0)' || thisNodeBackgroundColor == 'rgb(0, 0, 0)') {
+            domWalker.currentNode.style.backgroundColor = '#171717';
+        }
+        if (thisNodeBackground == 'rgb(23, 23, 23)' || thisNodeBackgroundColor == 'rgb(23, 23, 23)') {
+            domWalker.currentNode.style.backgroundColor = '#000000';
+        }
+    }
+
+    function checkColor(node) {
+
+        var thisNodeColor = window.getComputedStyle(node)['color'];
+
+        if (thisNodeColor == 'rgb(255, 255, 255)') {
+            domWalker.currentNode.style.color = '#FAFAFA';
+        }
+        if (thisNodeColor == 'rgb(250, 250, 250)') {
+            domWalker.currentNode.style.color = '#FFFFFF';
+        }
+        if (thisNodeColor == 'rgb(0, 0, 0)') {
+            domWalker.currentNode.style.color = '#171717';
+        }
+        if (thisNodeColor == 'rgb(23, 23, 23)') {
+            domWalker.currentNode.style.color = "#000000";
+        }
+
+    }
+
+    function checkLineHeight(node) {
+        var thisNodeLineHeight = window.getComputedStyle(node)['line-height'];
+
+        if(thisNodeLineHeight == 'normal'){
+        	domWalker.currentNode.style.lineHeight = '1.3';
+        }
+    }
+}
